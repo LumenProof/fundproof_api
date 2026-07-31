c1064d4e1ad024e137fa03714469575267541e46
nafiuishaaq <nafiuig@gmail.com>
2026-07-31 13:52:21 +0100

    implemeneted the rate limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ThrottlerModule, ThrottlerGuard } from 'nestjs-throttler';
+import { APP_GUARD } from '@nestjs/core';
 import { FundProofController } from './fundproof.controller';
 import { FundProofService } from './fundproof.service';
 import { StoredAttestation } from './fundproof.entity';
@@ -15,8 +17,18 @@ import { StoredAttestation } from './fundproof.entity';
       synchronize: true,
     }),
     TypeOrmModule.forFeature([StoredAttestation]),
+    ThrottlerModule.forRoot([{
+      ttl: 60,
+      limit: 20,
+    }]),
   ],
   controllers: [FundProofController],
-  providers: [FundProofService],
+  providers: [
+    FundProofService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
