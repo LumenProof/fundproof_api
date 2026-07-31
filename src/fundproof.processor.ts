25ec1bc26b41221569ec97028529cef460b0cb2b
nafiuishaaq <nafiuig@gmail.com>
2026-07-31 13:53:51 +0100

    implemeneted the rate limit

---
+++ src/fundproof.processor.ts
@@ -0,0 +1,13 @@
+import { Processor, Process } from '@nestjs/bull';
+import { Job } from 'bull';
+import { FundProofService } from './fundproof.service';
+
+@Processor('proof-generation')
+export class FundProofProcessor {
+  constructor(private readonly fundProofService: FundProofService) {}
+
+  @Process()
+  async transcode(job: Job<{ attestationId: string }>) {
+    await this.fundProofService.generateProof(job.data.attestationId);
+  }
+}
