261abb7ae3a2e919e8ea7f3ff4b95a9a15a15006
nafiuishaaq <nafiuig@gmail.com>
2026-07-31 13:48:16 +0100

    implemeneted the main app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { FundProofController } from './fundproof.controller';
 import { FundProofService } from './fundproof.service';
+import { StoredAttestation } from './fundproof.entity';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'fundproof.db',
+      entities: [StoredAttestation],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([StoredAttestation]),
+  ],
   controllers: [FundProofController],
   providers: [FundProofService],
 })
